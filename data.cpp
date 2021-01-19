2b5b7deae11cbc7064161f2dd3f41e22586afb7c
Virtual-Y-Monster <1113459606@qq.com>
2021-01-20 00:28:00 +0800

    修改文件换行符

--- data.cpp
+++ data.cpp
@@ -1,9 +1,19 @@
 #include "data.h"
 int cmdNum = 0; // 命令个数
-char commands[CMDSIZE][CMDSIZE] = { 0 }; // 命令
+char commands[CMDSIZE][CMDSIZE] = { 0 }; // 命令个数
 char promptSign = '$'; // 提示符
 extern char promptPath[PATH_MAX] = "\0"; // 提示路径
 extern struct passwd* user_info = NULL; // 用户信息
 char curPath[PATH_MAX] = "\0"; // 工作路径
 char username[CMDSIZE] = "\0"; // 用户名
 char hostname[CMDSIZE] = "\0"; // 主机名
+
+const char CMD_CD[] = "cd"; // cd
+const char CMD_PWD[] = "pwd"; // pwd
+const char CMD_LS[] = "ls"; // ls
+const char CMD_ECHO[] = "echo"; // echo
+const char CMD_CAT[] = "cat"; // cat
+const char CMD_EXIT[] = "exit"; // exit
+const char CMD_PIPE[] = "|"; // |
+const char CMD_IN[] = "<"; // <
+const char CMD_OUT[] = ">"; // >
--- data.h
+++ data.h
@@ -4,11 +4,41 @@
 #include <sys/types.h>
 #define CMDSIZE 256
 
-extern int cmdNum;
-extern char commands[][CMDSIZE];
-extern char promptSign;
-extern char promptPath[];
-extern struct passwd* user_info;
-extern char curPath[];
-extern char username[];
-extern char hostname[];
+extern int cmdNum; // 命令个数
+extern char commands[][CMDSIZE]; // 命令个数
+extern char promptSign; // 提示符
+extern char promptPath[]; // 提示路径
+extern struct passwd* user_info; // 用户信息
+extern char curPath[]; // 工作路径
+extern char username[]; // 用户名
+extern char hostname[]; // 主机名
+
+extern const char CMD_CD[]; // cd
+extern const char CMD_PWD[]; // pwd
+extern const char CMD_LS[]; // ls
+extern const char CMD_ECHO[]; // echo
+extern const char CMD_CAT[]; // cat
+extern const char CMD_EXIT[]; // exit
+extern const char CMD_PIPE[]; // |
+extern const char CMD_IN[]; // <
+extern const char CMD_OUT[]; // >
+
+//状态码
+enum
+{
+	EXIT,
+	RIGHT,
+	ERROR_COMMAND,
+	//ERROR_CD,
+	//ERROR_PWD,
+	//ERROR_LS,
+	//ERROR_ECHO,
+	//ERROR_CAT,
+	//ERROR_EXIT,
+	ERROR_WRONG_PARAMETER,
+	ERROR_MISS_PARAMETER,
+	ERROR_TOO_MANY_PARAMETER,
+	SIMPLE,
+	COMPLEX,
+	ERROR_PATH,
+};
