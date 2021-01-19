a3266c272cd3f4e405d18ec910f266852cc60dbf
Virtual-Y-Monster <1113459606@qq.com>
2021-01-19 13:16:31 +0800

    添加注释

--- prompt.h
+++ prompt.h
@@ -7,8 +7,13 @@
 #include <stdio.h>
 #include "data.h"
 
+//获取用户名
 void getUserName();
+//获取主机名
 void getHostName();
+//获取提示符
 void getPromptSign();
+//获取提示路径
 void getPromptPath();
+//打印提示
 void printPrompt();
